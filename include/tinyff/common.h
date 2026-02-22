c9977fd038d10c50abcfa2f92e07bc5401737bec
TazyFoundSoup <gardendistrict0x0@outlook.com>
2026-02-22 15:38:13 +1100

    fix(common): Move inline endian utility functions to common.h

--- include/tinyff/common.h
+++ include/tinyff/common.h
@@ -14,7 +14,19 @@ typedef bool ff_flag;
 
 // Functions
 
-inline uint32_t get_big_endian(const uint8_t *buffer);
-inline uint32_t get_little_endian(const uint8_t *buffer);
+static inline uint32_t get_big_endian(const uint8_t *buffer) {
+    return (uint32_t)(buffer[0] << 24) |
+           (uint32_t)(buffer[1] << 16) |
+           (uint32_t)(buffer[2] << 8)  |
+           (uint32_t)(buffer[3]);
+}
+
+static inline uint32_t get_little_endian(const uint8_t *buffer)
+{
+    return (uint32_t)(buffer[0])       |
+           (uint32_t)(buffer[1] << 8)  |
+           (uint32_t)(buffer[2] << 16) |
+           (uint32_t)(buffer[3] << 24);
+}
 
 #endif
--- src/common.c
+++ src/common.c
@@ -1,16 +1,3 @@
 #include "tinyff/common.h"
 
-inline uint32_t get_big_endian(const uint8_t *buffer) {
-    return (uint32_t)(buffer[0] << 24) |
-           (uint32_t)(buffer[1] << 16) |
-           (uint32_t)(buffer[2] << 8)  |
-           (uint32_t)(buffer[3]);
-}
-
-inline uint32_t get_little_endian(const uint8_t *buffer)
-{
-    return (uint32_t)(buffer[0])       |
-           (uint32_t)(buffer[1] << 8)  |
-           (uint32_t)(buffer[2] << 16) |
-           (uint32_t)(buffer[3] << 24);
-}
+// Endian functions moved to header because C's acting up again
