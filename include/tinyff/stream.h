21e38c781e9d9b392d3e07371b49fae709c0fec3
TazyFoundSoup <gardendistrict0x0@outlook.com>
2026-03-28 16:28:44 +1100

    fix(stream): Remove old file code in stream.h

--- include/tinyff/stream.h
+++ include/tinyff/stream.h
@@ -18,10 +18,4 @@ typedef struct {
     void *user;
 } ff_stream;
 
-// Default reads for common types (just FILE for now, memory later)
-size_t ff_file_read(void *ptr, size_t size, void *user);
-
-// Stream creation helpers
-ff_stream ff_create_file_stream(FILE *f);
-
 #endif 
