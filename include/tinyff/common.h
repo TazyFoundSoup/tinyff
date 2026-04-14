b30e14c73acee81b9667667e958986a940883c91
TazyFoundSoup <gardendistrict0x0@outlook.com>
2026-04-14 10:12:00 +1000

    feat(common): Add ff_memcmp

--- include/tinyff/common.h
+++ include/tinyff/common.h
@@ -41,6 +41,15 @@ static inline size_t ff_strlen(const char *str)
     return len;
 }
 
+static inline int ff_memcmp(const void *a, const void *b, size_t n)
+{
+    const uint8_t *pa = (const uint8_t *)a;
+    const uint8_t *pb = (const uint8_t *)b;
+    for (size_t i = 0; i < n; i++) {
+        if (pa[i] != pb[i]) return pa[i] - pb[i];
+    }
+    return 0;
+}
 
 // Context bases
 // Default
