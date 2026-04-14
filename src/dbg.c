097ad322e0cbe5ca72a3d7c1938f2925547c196b
TazyFoundSoup <gardendistrict0x0@outlook.com>
2026-04-14 11:08:56 +1000

    fix(dbg): ff_dprintf null write callback check

--- src/dbg.c
+++ src/dbg.c
@@ -1,23 +1,20 @@
+#include <tinyff/stream.h>
 #include <tinyff/dbg.h>
 
 ff_result ff_set_debug_stream(ff_stream* stream, ff_ctx* ctx)
 {
-    ctx->ff_debug_stream = stream;
+    ctx->ff_debug_stream = *stream;
     ctx->ff_debug_enabled = (stream != NULL);
 
-    ff_dprintf(ctx, "tinyff 0.1.0 debug stream\n");
-    ff_dprintf(ctx, "For more information, visit\n");
-    ff_dprintf(ctx, "https://github.com/TazyFoundSoup/tinyff\n");
-    ff_dprintf(ctx, "------------------------------\n");
-
+    // TODO: Add banner back maybe
+    
     return FF_RESULT_OK;
 }
 
 ff_result ff_dprintf(ff_ctx* ctx, const char *msg)
 {
-    if (!ctx->ff_debug_enabled || !ctx->ff_debug_stream) return FF_RESULT_WARN_DEBUG_DISABLED;
-
-    ctx->ff_debug_stream->write(msg, ff_strlen(msg), ctx->ff_debug_stream->user);
-
+    if (!ctx->ff_debug_enabled) return FF_RESULT_WARN_DEBUG_DISABLED;
+    if (!ctx->ff_debug_stream.write) return FF_RESULT_WARN_DEBUG_DISABLED;
+    ctx->ff_debug_stream.write(msg, ff_strlen(msg), ctx->ff_debug_stream.user);
     return FF_RESULT_OK;
 }
